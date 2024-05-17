6afa04f160b5baac030665a8dbd02522bea7a6b8
Sirpip91 <27028592+Sirpip91@users.noreply.github.com>
2024-05-17 15:19:24 -0500

    main page updates

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,15 +10,15 @@ const Main = () => {
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
           <p className='uppercase text-sm tracking-widest text-gray-600'>
-            LET&#39;S CODE
+            LET&#39;S PROGRAM
           </p>
           <h1 className='py-4 text-gray-700'>
             Hi, I&#39;m <span className='text-[#0e6fff]'> Bradley</span>
           </h1>
           <h1 className='py-2 text-gray-700'>A Software Engineer</h1>
           <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
-            I’m focused on building responsive front-end web applications while
-            learning low-level programming concepts.
+            I’m focused on building software solutions that have an impact while providing value.
+            
           </p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
             <a
